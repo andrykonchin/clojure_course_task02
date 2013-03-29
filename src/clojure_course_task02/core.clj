daf9653f955785e7ab6e91b9cfd98e5f1ec26df2
Andry Konchin <andry.konchin@gmail.com>
2013-03-29 19:53:54 +0200

    Добавил решение с pmap.

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,10 +1,16 @@
 (ns clojure-course-task02.core
   (:gen-class))
 
+(import java.io.File)
 
 (defn find-files [file-name path]
-  "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+  (let [f (File. path)]
+    (if (.isFile f)
+      (if (not (empty? (re-seq (re-pattern file-name) (. f getName))))
+        [(. f getName)]
+        [])
+      (reduce concat (doall (pmap (fn [s] (find-files file-name (str path (. File separator ) s)))
+                                  (. f list)))))))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
@@ -15,4 +21,5 @@
     (usage)
     (do
       (println "Searching for " file-name " in " path "...")
-      (dorun (map println (find-files file-name path))))))
+      (dorun (map println (find-files file-name path)))))
+  (shutdown-agents))
